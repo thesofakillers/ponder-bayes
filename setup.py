b2317fb5141134c85dbc7640f845210db4af5e9b
Giulio Starace <giulio.starace@gmail.com>
2022-05-05 10:41:25 +0200

    project skeleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Official repository for the paper "PonderBayes: Rationally Pondering Neural Networks"',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
