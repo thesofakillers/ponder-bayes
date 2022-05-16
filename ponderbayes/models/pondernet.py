52f4257a1bc0868632043b7740a86e44a3b5c1df
Giulio Starace <giulio.starace@gmail.com>
2022-05-16 11:13:15 +0200

    port pondernet to pt lightning

--- ponderbayes/models/pondernet.py
+++ ponderbayes/models/pondernet.py
@@ -2,11 +2,16 @@
 Credit for PonderNet, ReconstructionLoss and RegularizationLoss largely to
 https://github.com/jankrepl/mildlyoverfitted/tree/master/github_adventures/pondernet
 """
+from functools import partial
+
+import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 
+from ponderbayes.utils import metrics
 
-class PonderNet(nn.Module):
+
+class PonderNet(pl.LightningModule):
     """Network that ponders.
 
     Parameters
@@ -23,32 +28,40 @@ class PonderNet(nn.Module):
     allow_halting : bool
         If True, then the forward pass is allowed to halt before
         reaching the maximum steps.
-
-    Attributes
-    ----------
-    cell : nn.GRUCell
-        Learnable GRU cell that maps the previous hidden state and the input
-        to a new hidden state.
-
-    output_layer : nn.Linear
-        Linear module that serves as the binary classifier. It inputs
-        the hidden state.
-
-    lambda_layer : nn.Linear
-        Linear module that generates the halting probability at each step.
-
+    beta : float
+        Hyperparameter governing the regularization loss
+    lambda_p : float
+        Hyperparameter governing the probability of halting
     """
 
-    def __init__(self, n_elems, n_hidden=64, max_steps=20, allow_halting=False):
+    def __init__(
+        self,
+        n_elems,
+        n_hidden=64,
+        max_steps=20,
+        allow_halting=False,
+        beta=0.01,
+        lambda_p=0.4,
+    ):
         super().__init__()
 
-        self.max_steps = max_steps
+        self.n_elems = n_elems
         self.n_hidden = n_hidden
+        self.max_steps = max_steps
         self.allow_halting = allow_halting
+        self.beta = beta
+        self.lambda_p = lambda_p
 
         self.cell = nn.GRUCell(n_elems, n_hidden)
         self.output_layer = nn.Linear(n_hidden, 1)
         self.lambda_layer = nn.Linear(n_hidden, 1)
+
+        self.loss_rec_inst = ReconstructionLoss(nn.BCEWithLogitsLoss(reduction="none"))
+        self.loss_reg_inst = RegularizationLoss(
+            lambda_p=self.lambda_p, max_steps=self.max_steps
+        )
+
+        self.save_hyperparameters()
 
     def forward(self, x):
         """Run forward pass.
@@ -120,6 +133,94 @@ class PonderNet(nn.Module):
         p = torch.stack(p_list)
 
         return y, p, halting_step
+
+    def _accuracy_step(self, y_pred_batch, y_true_batch, halting_step):
+        """computes accuracy metrics for a given batch"""
+        # (batch_size,) the prediction where the model halted
+        y_halted_batch = y_pred_batch.gather(
+            dim=0,
+            index=halting_step[None, :] - 1,
+        )[0]
+        # (scalar), the accuracy at the halted step
+        accuracy_halted_step = metrics.accuracy(
+            y_halted_batch, y_true_batch, threshold=0
+        )
+        # (max_steps, ), the accuracy at each step
+        accuracy_all_steps = metrics.accuracy(
+            y_pred_batch, y_true_batch, threshold=0, dim=1
+        )
+        return accuracy_halted_step, accuracy_all_steps
+
+    def _loss_step(self, p, y_pred_batch, y_true_batch):
+        """computes the loss for a given batch"""
+        # reconstruction loss
+        loss_rec = self.loss_rec_inst(
+            p,
+            y_pred_batch,
+            y_true_batch,
+        )
+        # regularization loss
+        loss_reg = self.loss_reg_inst(
+            p,
+        )
+        # overall loss
+        loss_overall = loss_rec + self.beta * loss_reg
+        return loss_rec, loss_reg, loss_overall
+
+    def _shared_step(self, batch, batch_idx, phase):
+        """runs forward, computes accuracy and loss and logs"""
+        # (batch_size, n_elems), (batch_size,)
+        x_batch, y_true_batch = batch
+        # (max_steps, batch_size), (max_steps, batch_size), (batch_size,)
+        y_pred_batch, p, halting_step = self(x_batch)
+        # batch accuracy at the halted step, batch accuracy at each step
+        accuracy_halted_step, accuracy_all_steps = self._accuracy_step(
+            y_pred_batch, y_true_batch, halting_step
+        )
+        # reconstruction, regularization and overall loss
+        loss_rec, loss_reg, loss_overall = self._loss_step(
+            p, y_pred_batch, y_true_batch
+        )
+        # collating all results
+        results = {
+            "halting_step": halting_step,
+            "p": p,
+            "accuracy_halted_step": accuracy_halted_step,
+            "accuracy_all_steps": accuracy_all_steps,
+            "loss_rec": loss_rec,
+            "loss_reg": loss_reg,
+            "loss_overall": loss_overall,
+        }
+        # logging
+        self.log_dict(
+            {
+                f"{phase}-{k}": results[k]
+                for k in [
+                    "loss_rec",
+                    "loss_reg",
+                    "loss_overall",
+                    "halting_step",
+                    "accuracy_halted_step",
+                ]
+            }
+        )
+        # need to force dimension 0 mean for per-step metrics
+        self.log_dict(
+            {f"{phase}-{k}": results[k] for k in ["p", "accuracy_all_steps"]},
+            reduce_fx=partial(torch.mean, dim=0),
+        )
+        # needed for backward
+        return loss_overall
+
+    def training_step(self, batch, batch_idx):
+        loss_overall = self._shared_step(batch, batch_idx, "train")
+        return loss_overall
+
+    def validation_step(self, batch, batch_idx):
+        self._shared_step(batch, batch_idx, "val")
+
+    def test_step(self, batch, batch_idx):
+        self._shared_step(batch, batch_idx, "test")
 
 
 class ReconstructionLoss(nn.Module):
