7611598a858fcb9e15949883da78d7e0746abd98
Giulio Starace <giulio.starace@gmail.com>
2022-05-26 20:57:04 +0200

    working version of groupthink

--- ponderbayes/models/groupthink.py
+++ ponderbayes/models/groupthink.py
@@ -1,9 +1,13 @@
 import torch
+import torch.nn as nn
+import pytorch_lightning as pl
 
-from ponderbayes.models.pondernet import PonderNetModule, PonderNet
+from ponderbayes.models.pondernet import PonderNetModule
+from ponderbayes.models import losses
+from ponderbayes import utils
 
 
-class GroupThink(PonderNet):
+class GroupThink(pl.LightningModule):
     """
     PonderNets Together Strong.
 
@@ -33,31 +37,58 @@ class GroupThink(PonderNet):
         beta=0.01,
         lambda_p=0.4,
     ):
-        super().__init__(n_elems, n_hidden, max_steps, allow_halting, beta, lambda_p)
+        super().__init__()
+
+        self.n_elems = n_elems
+        self.n_hidden = n_hidden
+        self.max_steps = max_steps
+        self.allow_halting = allow_halting
+        self.beta = beta
+        self.lambda_p = lambda_p
+
         self.ensemble_size = ensemble_size
-        self.ensemble = []
-        for _ in range(ensemble_size):
-            pondernet = PonderNetModule(n_elems, n_hidden, max_steps, allow_halting)
-            self.ensemble.append(pondernet)
+        self.ensemble = nn.ModuleList(
+            [
+                PonderNetModule(n_elems, n_hidden, max_steps, allow_halting)
+                for _ in range(ensemble_size)
+            ]
+        )
+        # can't rely on automatic optimization from pl here
+        self.automatic_optimization = False
+
+        self.loss_rec_inst = losses.ReconstructionLoss(
+            nn.BCEWithLogitsLoss(reduction="none")
+        ).to(torch.float32)
+        self.loss_reg_inst = losses.RegularizationLoss(
+            lambda_p=self.lambda_p, max_steps=self.max_steps
+        ).to(torch.float32)
+
+        self.save_hyperparameters()
 
     def forward(self, x):
         """
+        Forward pass, convenient for inference, but not actually used for
+        training
 
         Parameters
         ----------
         x : torch.Tensor
             Batch of input features of shape `(batch_size, n_elems)`.
         """
-        batch_size = x.shape[0]
-        y_ensemble = torch.zeros(self.ensemble_size, self.max_steps, batch_size)
-        p_ensemble = torch.zeros(self.ensemble_size, self.max_steps, batch_size)
-        halting_step_ensemble = torch.zeros(self.ensemble_size, batch_size)
+        y_list = []
+        p_list = []
+        halting_step_list = []
 
-        for i, pondernet in enumerate(self.ensemble):
-            y, p, halting_step = pondernet(x)
-            y_ensemble[i] = y
-            p_ensemble[i] = p
-            halting_step_ensemble[i] = halting_step
+        for i in range(self.ensemble_size):
+            y, p, halting_step = self.ensemble[i](x)
+            y_list.append(y)
+            p_list.append(p)
+            halting_step_list.append(halting_step)
+
+        # stack the lists of tensors into single tensor of shape (ensemble_size, max_steps, batch_size)
+        y_ensemble = torch.stack(y_list, dim=0)
+        p_ensemble = torch.stack(p_list, dim=0)
+        halting_step_ensemble = torch.stack(halting_step_list, dim=0)
 
         return y_ensemble, p_ensemble, halting_step_ensemble
 
@@ -68,79 +99,103 @@ class GroupThink(PonderNet):
         The y_preds, p and halting_steps are averaged across the ensemble
         """
         x, y_true = batch
+        batch_size = x.shape[0]
         y_true = y_true.double()
 
-        # same as tensors below, but additional first dimension of ensemble_size
-        y_pred_ensemble, p_ensemble, halting_step_ensemble = self(x)
+        # accuracy at the halting step for each model
+        accuracies = torch.zeros(self.ensemble_size, batch_size, requires_grad=False)
 
-        # (max_steps, batch_size)
-        y_pred_mean = torch.mean(y_pred_ensemble, dim=0)
-        y_pred_std = torch.std(y_pred_ensemble, dim=0)
+        # loss for each model
+        losses_rec = torch.zeros(self.ensemble_size, requires_grad=False)
+        losses_reg = torch.zeros(self.ensemble_size, requires_grad=False)
+        losses_overall = torch.zeros(self.ensemble_size, requires_grad=False)
 
-        # (max_steps, batch_size)
-        std_err = y_pred_std / torch.sqrt(torch.Tensor([self.ensemble_size]))
+        # (sigmoid-scaled) y_pred at halting step for each model
+        y_pred_halted_ensemble = torch.zeros(self.ensemble_size, batch_size)
+        # halting step of each model
+        halting_step_ensemble = torch.zeros(self.ensemble_size, batch_size)
 
-        # (max_steps, batch_size)
-        p_mean = torch.mean(p_ensemble, dim=0)
-        p_std = torch.std(p_ensemble, dim=0)
+        # perform forward and evaluate each model
+        for i, model in enumerate(self.ensemble):
+            # forward pass
+            y_pred, p, halting_step = model(x)
+            y_pred_halted = utils.tensor_at_halting_step(y_pred, halting_step.long())
+            # accuracy
+            accuracy = self._accuracy_step(y_pred_halted, y_true)
+            accuracies[i] = accuracy
+            # apply sigmoid to normalize our output for valid std_err calculation
+            with torch.no_grad():
+                y_pred_halted_scaled = torch.sigmoid(y_pred_halted).detach()
+            y_pred_halted_ensemble[i] = y_pred_halted_scaled
+            halting_step_ensemble[i] = halting_step
+            # losses
+            if phase == "train":
+                opt = self.optimizers()[i]
+                opt.zero_grad()
+            loss_rec, loss_reg, loss_overall = self._loss_step(p, y_pred, y_true)
+            # optimization per model
+            if phase == "train":
+                self.manual_backward(loss_overall)
+                opt.step()
+            losses_rec[i] = loss_rec
+            losses_reg[i] = loss_reg
+            losses_overall[i] = loss_overall
 
-        # (batch_size, )
-        # TODO: don't do this,
-        # compute the accuracy for each model at the relevant halting step
-        # and average that
-        # same for std_err
-        halting_step_mean = torch.mean(halting_step_ensemble, dim=0).floor().long()
-        halting_step_std = torch.std(halting_step_ensemble, dim=0)
-
-        # index std_err by halting step mean, to get (batch_size, ) tensor
-        std_err_halting_step = std_err.gather(
-            dim=0, index=halting_step_mean[None, :] - 1
-        ).squeeze()
-
-        accuracy_halted_step, accuracy_all_steps = self._accuracy_step(
-            y_pred_mean, y_true, halting_step_mean
-        )
-        # currently taking the loss of the mean
-        # if that doesn't work, try taking the loss of each element in the ensemble
-        loss_rec, loss_reg, loss_overall = self._loss_step(p_mean, y_pred_mean, y_true)
+        # get mean accuracy
+        accuracy_halted_step = torch.mean(accuracies, dim=0)
+        # get std err of (sigmoid-scaled) prediction
+        std_dev_halted = torch.std(y_pred_halted_ensemble, dim=0)
+        std_err_halted = std_dev_halted / torch.sqrt(torch.Tensor([self.ensemble_size]))
 
         # collating all results
         results = {
-            "halting_step": halting_step_mean.double().mean(),
-            "p": p_mean.mean(dim=1),
-            "accuracy_halted_step": accuracy_halted_step,
-            "accuracy_all_steps": accuracy_all_steps,
-            "loss_rec": loss_rec,
-            "loss_reg": loss_reg,
-            "loss": loss_overall,
-            "std_err": std_err.mean(dim=1),
-            "std_err_halting_step": std_err_halting_step.mean(),
+            "halting_step": halting_step_ensemble.double().mean(),
+            "accuracy_halted_step": accuracy_halted_step.mean(),
+            "loss_rec": losses_rec.mean(dim=0),
+            "loss_reg": losses_reg.mean(dim=0),
+            "loss": losses_overall.mean(dim=0),
+            "std_err": std_err_halted.mean(),
         }
-        # logging; p and accuracy_all_steps logged in _shared_epoch_end
-        self.log_dict(
-            {
-                f"{phase}/{k}": results[k]
-                for k in results.keys()
-                if k not in ["accuracy_all_steps", "p", "std_err"]
-            }
-        )
-        # needed for backward
+        self.log_dict({f"{phase}/{k}": v for k, v in results.items()})
         return results
 
-    # def _shared_epoch_end(self, outputs, phase):
-    #     """
-    #     Accumulates and logs per-step metrics at the end of the epoch
+    def training_step(self, batch, batch_idx):
+        return self._shared_step(batch, batch_idx, "train")
 
-    #     Each metric is averaged across the batches
-    #     """
-    #     accuracy_all_steps = torch.stack(
-    #         [output["accuracy_all_steps"] for output in outputs]
-    #     ).mean(dim=0)
-    #     p = torch.stack([output["p"] for output in outputs]).mean(dim=0)
-    #     std_err = torch.stack([output["std_err"] for output in outputs]).mean(dim=0)
-    #     for i, (accuracy, step_p, step_std_err) in enumerate(
-    #         zip(accuracy_all_steps, p, std_err), start=1
-    #     ):
-    #         self.log(f"{phase}/step_accuracy/{i}", accuracy)
-    #         self.log(f"{phase}/step_p/{i}", step_p)
-    #         self.log(f"{phase}/step_std_err/{i}", step_std_err)
+    def validation_step(self, batch, batch_idx):
+        return self._shared_step(batch, batch_idx, "val")
+
+    def test_step(self, batch, batch_idx):
+        return self._shared_step(batch, batch_idx, "test")
+
+    def _accuracy_step(self, y_pred_halted, y_true):
+        accuracy_halted_step = utils.metrics.accuracy(
+            y_pred_halted, y_true, threshold=0
+        )
+        return accuracy_halted_step
+
+    def _loss_step(self, p, y_pred_batch, y_true_batch):
+        """computes the loss for a given batch"""
+        # reconstruction loss
+        loss_rec = self.loss_rec_inst(
+            p,
+            y_pred_batch,
+            y_true_batch,
+        )
+        # regularization loss
+        loss_reg = self.loss_reg_inst(
+            p,
+        )
+        # overall loss
+        loss_overall = loss_rec + self.beta * loss_reg
+        return loss_rec, loss_reg, loss_overall
+
+    def configure_optimizers(self):
+        optimizers = []
+        for model in self.ensemble:
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.0003)
+            optimizers.append(optimizer)
+        return optimizers
+
+    def configure_gradient_clipping(self, optimizer, optimizer_idx):
+        self.clip_gradients(optimizer, 1, "norm")
